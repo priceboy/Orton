18856a05eacded0bdce8b5abb4799a59ed7b83c1
priceboy <psheutang@gmail.com>
2026-05-01 18:31:48 +0100

    fix duplicate PORT declaration

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 app.use("/upload", uploadRoute);
 app.use("/search", searchRoute);
 
-const PORT = 3000;
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
