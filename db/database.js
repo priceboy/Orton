88db49c58dba9771c6afe3eed339363c4292e266
priceboy <psheutang@gmail.com>
2026-05-29 01:15:22 +0100

    Update database.js

--- db/database.js
+++ db/database.js
@@ -1,16 +1,56 @@
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./tickets.db", (err) => {
+const path = require("path");
 
-    if (err) {
+const fs = require("fs");
 
-        console.error("Database error:", err.message);
+// ======================================
+// DATABASE PATH
+// ======================================
 
-    } else {
+const dbPath = path.join(
+    __dirname,
+    "../tickets.db"
+);
 
-        console.log("Connected to SQLite database");
+console.log("DB PATH:", dbPath);
+
+// ======================================
+// CREATE DB FILE IF MISSING
+// ======================================
+
+if (!fs.existsSync(dbPath)) {
+
+    fs.writeFileSync(dbPath, "");
+
+    console.log("tickets.db CREATED");
+}
+
+// ======================================
+// CONNECT DATABASE
+// ======================================
+
+const db = new sqlite3.Database(
+
+    dbPath,
+
+    (err) => {
+
+        if (err) {
+
+            console.error(
+                "DB CONNECTION ERROR:",
+                err.message
+            );
+
+        } else {
+
+            console.log(
+                "Connected to tickets database"
+            );
+        }
     }
-});
+);
 
 // ======================================
 // CREATE TABLE
@@ -19,16 +59,23 @@ const db = new sqlite3.Database("./tickets.db", (err) => {
 db.serialize(() => {
 
     db.run(`
+
         CREATE TABLE IF NOT EXISTS tickets (
 
             id INTEGER PRIMARY KEY AUTOINCREMENT,
 
             customer_name TEXT,
 
+            phone TEXT,
+
+            reference TEXT,
+
             departure_airport TEXT,
+
             arrival_airport TEXT,
 
             departure_date TEXT,
+
             departure_time TEXT,
 
             checkin_time TEXT,
@@ -38,29 +85,39 @@ db.serialize(() => {
             flight_number TEXT,
 
             cabin_luggage TEXT,
+
             checked_luggage TEXT,
 
             trip_type TEXT,
 
             return_departure_airport TEXT,
+
             return_arrival_airport TEXT,
 
             return_departure_date TEXT,
+
             return_departure_time TEXT,
 
             return_checkin_time TEXT,
 
             return_flight_number TEXT
+
         )
+
     `, (err) => {
 
         if (err) {
 
-            console.log("Table creation failed:", err);
+            console.log(
+                "TABLE ERROR:",
+                err
+            );
 
         } else {
 
-            console.log("tickets table ready");
+            console.log(
+                "tickets table ready"
+            );
         }
     });
 });
