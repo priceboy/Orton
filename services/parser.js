7f6742ad302af9a5ae0b5875f787bd07e03da3c9
priceboy <psheutang@gmail.com>
2026-05-31 12:48:24 +0100

    Update parser.js

--- services/parser.js
+++ services/parser.js
@@ -13,7 +13,25 @@ function getCheckInTime(time) {
         .padStart(2, "0")}`;
 }
 
-function cleanText(text) {
+function cleanText(text)
+const VALID_AIRPORTS = [
+
+    "NSI",
+    "DLA",
+    "CDG",
+    "KGL",
+    "BGF",
+    "ADD",
+    "IST",
+    "BRU",
+    "LHR",
+    "DXB",
+    "JFK",
+    "AMS",
+    "FRA",
+    "CMN"
+
+];{
 
     return text
         .replace(/\r/g, "\n")
@@ -41,7 +59,29 @@ function extractName(text) {
         "TERMINAL",
         "ECONOMY"
     ];
+    // ======================================
+    // PREPARED FOR
+    // ======================================
 
+    const preparedForMatch = text.match(
+
+        /PREPARED FOR\s*\n\s*([^\n]+)/i
+
+    );
+
+    if (preparedForMatch?.[1]) {
+
+        const preparedName =
+            preparedForMatch[1]
+
+            .replace(/\s+/g, " ")
+            .trim();
+
+        if (preparedName.length > 3) {
+
+            return preparedName;
+        }
+    }
     // ======================================
     // ORIGINAL WORKING FORMAT
     // ======================================
